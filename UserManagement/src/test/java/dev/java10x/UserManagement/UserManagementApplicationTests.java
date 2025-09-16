686dd5fbec1e6afb9a57e5ef93715fca29deb6cf
Leo <leo.sato957@gmail.com>
2025-09-16 19:31:59 -0300

    first commit

---
+++ UserManagement/src/test/java/dev/java10x/UserManagement/UserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.UserManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
